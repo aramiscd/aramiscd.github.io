279674a87a9c49b93a6dd07b1d50879b05044b98
Aramís Concepción Durán <aramis@systemli.org>
2024-02-20 01:14:13 +0100

    update

--- push.fish
+++ push.fish
@@ -4,8 +4,8 @@ hugo -d docs
 echo -n www.nojs.de > docs/CNAME
 git add --all
 git commit --message=update
-# git push git@git.eee.gg:aramis/nojs.de
-# git push git@git.sr.ht:~aramis/nojs.de
-# git push git@github.com:aramiscd/nojs.de
+git push git@git.eee.gg:aramis/nojs.de
+git push git@git.sr.ht:~aramis/nojs.de
+git push git@github.com:aramiscd/nojs.de
 rm -rf resources
 
